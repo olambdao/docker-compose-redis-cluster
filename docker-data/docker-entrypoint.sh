e86b3b96d8569de7c577615c4a6820d544f5a09a
olambdao <olambdao@gmail.com>
2020-03-10 10:38:30 +0900

    Use redis:alpine instead of redis:3

--- docker-data/docker-entrypoint.sh
+++ docker-data/docker-entrypoint.sh
@@ -2,7 +2,9 @@
 
 if [ "$1" = 'redis-cluster' ]; then
     sleep 10
-    echo "yes" | ruby /redis/src/redis-trib.rb create --replicas 0 173.17.0.2:7000 173.17.0.3:7001 173.17.0.4:7002
+    redis-cli --cluster call 173.17.0.2:7000 flushall
+    redis-cli --cluster call 173.17.0.2:7000 cluster reset
+    echo "yes" | redis-cli --cluster create 173.17.0.2:7000 173.17.0.3:7001 173.17.0.4:7002 --cluster-replicas 0
     echo "DONE"
 else
   exec "$@"
